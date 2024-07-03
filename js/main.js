98b9c3a9d05c002ffce0fadbd1fbd700f451c3f0
Md.Mainul Hasan <mdevhasan@gmail.com>
2024-07-03 10:37:41 +0600

    nav bar

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
 // mobile nav
 
-const mobileNav = document.querySelector('.mnav')
-const closeBtn = document.querySelector('.mnav__close-btn')
-const closeBtnIcon = document.querySelector('.mnav__close-btn-icon')
+const mobileNav = document.querySelector(".mnav");
+const closeBtn = document.querySelector(".mnav__close-btn");
+const closeBtnIcon = document.querySelector(".mnav__close-btn-icon");
 
+const navOpenedClass = "left-0";
+const navCloseClass = "-left-[300px]";
+const arrowLeftClass = "ri-arrow-left-s-line";
+const arrowRightClass = "ri-arrow-right-s-line";
 
-const navOpenClass = 'left-0'
-const navCloseClass = '-left-[300px]'
-const arrowLeftClass = 'ri-arrow-left-s-line'
-const arrowRightClass = 'ri-arrow-right-s-line'
+closeBtn.addEventListener("click", () => {
+  console.log("clicked");
+  if (mobileNav.classList.contains(navCloseClass)) {
+    mobileNav.classList.toggle(navOpenedClass);
 
-
-closeBtn.addEventListener('click', () => {
-    console.log('clicked')
-    if (mobileNav.classList.contains(navCloseClass)) {
-        mobileNav.classList.toggle(navOpenClass)
-
-        closeBtnIcon.classList.toggle(arrowLeftClass)
-        closeBtn.classList.toggle(arrowRightClass)
-    }
-})
+    closeBtnIcon.classList.toggle(arrowLeftClass);
+    closeBtnIcon.classList.toggle(arrowRightClass);
+  }
+});
